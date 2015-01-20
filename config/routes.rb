a4d0daa5d96bfbc993ef5697c5738aebcd39c85e
Martin Heimbring <martin.heimbring@gmail.com>
2015-01-21 00:47:50 +0100

    allow a user to create a todo

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  resources :todos, only: [:index, :new, :create]
   root 'todos#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
